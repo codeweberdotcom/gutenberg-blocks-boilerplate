df0f34bca02b075162b9f61bae8f2692b15969c9
Denis Korablev <info@codeweber.com>
2024-11-06 14:38:44 +0300

    gradient colors js

---
+++ src/utilities/gradient_colors.js
@@ -0,0 +1,11 @@
+// src/utilities/colors.js
+export const gradientcolors = [
+	{ label: 'Gradient 1', value: 'gradient gradient-1' },
+	{ label: 'Gradient 2', value: 'gradient gradient-2' },
+	{ label: 'Gradient 3', value: 'gradient gradient-3' },
+	{ label: 'Gradient 4', value: 'gradient gradient-4' },
+	{ label: 'Gradient 5', value: 'gradient gradient-5' },
+	{ label: 'Gradient 6', value: 'gradient gradient-6' },
+	{ label: 'Gradient 7', value: 'gradient gradient-7' },
+	{ label: 'Gradient 8', value: 'gradient gradient-8' },
+];
